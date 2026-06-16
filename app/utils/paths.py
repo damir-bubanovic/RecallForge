d656914d3b1330a8d76665a044a949224a470735
Damir Bubanovic <damir.bubanovic@yahoo.com>
2026-06-16 18:30:18 +0200

    Fixed path for build of app

--- app/utils/paths.py
+++ app/utils/paths.py
@@ -2,12 +2,12 @@ from pathlib import Path
 import sys
 
 
-def get_base_path():
+def get_base_path() -> Path:
     if getattr(sys, "frozen", False):
-        return Path(sys.executable).resolve().parent.parent
+        return Path(getattr(sys, "_MEIPASS"))
 
     return Path(__file__).resolve().parents[2]
 
 
-def get_asset_path(filename: str):
+def get_asset_path(filename: str) -> str:
     return str(get_base_path() / "assets" / filename)
